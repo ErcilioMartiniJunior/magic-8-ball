c9134a5f22bea3351d9c4ea7e787218d6fcdbea8
Ercilio Martini Junior <erciliomartinijunior@gmail.com>
2023-03-23 23:35:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -39,4 +39,39 @@ switch(randomNumber) {
 
 console.log(`The Magic 8 ball says: ${eightBall}.`)
 
+/*
+
+Versão com prompt() e function()
+
+Nesta versão o código modificado usa o método prompt() para solicitar o nome do usuário e a pergunta, 
+tornando-o mais interativo e eliminando a necessidade de editar o código para inserir esses valores.
+
+O código modificado encapsula a lógica da Magic 8-Ball em uma função chamada getMagicEightBallAnswer(), 
+tornando-o mais modular e facilitando a reutilização e a manutenção:
+
+function getMagicEightBallAnswer() {
+  let randomNumber = Math.floor(Math.random() * 8);
+  
+  switch(randomNumber) {
+    case 0: return 'It is certain';
+    case 1: return 'It is decidedly so';
+    case 2: return 'Reply hazy try again';
+    case 3: return 'Cannot predict now';
+    case 4: return 'Do not count on it';
+    case 5: return 'My sources say no';
+    case 6: return 'Outlook not so good';
+    case 7: return 'Outlook not so good';
+  }
+}
+
+let userName = prompt("Please enter your name:", "");
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion = prompt("Please ask a question:", "");
+console.log(`${userName} has asked: ${userQuestion}`);
+
+let eightBall = getMagicEightBallAnswer();
+console.log(`The Magic 8 ball says: ${eightBall}.`);
+
+*/
 
